bf233a685f9763fe9526496bde018526e00670de
Jakub Dolejší <jakub.dolejsi3@gmail.com>
2019-11-27 01:35:51 +0100

    refaktor

--- Controllers/AdminController.php
+++ Controllers/AdminController.php
@@ -7,49 +7,48 @@ namespace Controllers;
 use Exceptions\CompleteRegistrationException;
 use Exceptions\DuplicateUser;
 use Exceptions\PasswordsAreNotSameException;
-use Exceptions\UpdateException;
-use Exceptions\UpdateSuccess;
-use PDO;
+
 
 class AdminController extends BaseController
 {
 
 	public function actionDefault(): void
 	{
-        try {
-            if (!isset($params[1])) {
-                $admin = $this->getModelFactory()->createAdminModel();
-                $users = $admin->process();
-                $this->loadView('admin');
-                $this->data['users'] = $users;
-            } elseif ($params[1] === 'add') {
-                $user = $this->getModelFactory()->createUserModel();
-                try{
-                    $user->register();
-                }
-                catch (PasswordsAreNotSameException $e){
-                    $this->alert($e->getMessage());
-                }
-                catch (DuplicateUser $e){
-                    $this->alert($e->getMessage());
-                }
-                catch (CompleteRegistrationException $e){
-                    $this->alert("");
-                }
-                $this->loadView('adminAddUser');
-            } elseif ($params[1] === 'edit') {
-                $this->loadView('adminEditUser');
-            } else {
-                $this->loadView('error404');
-            }
-        }
-        catch (UpdateSuccess $exception) {
-            $this->alert($exception->getMessage());
-            $this->redirect('admin');
-        }
-        catch (UpdateException $exception) {
-            $this->alert($exception->getMessage());
-        }
+		$admin = $this->getModelFactory()->createAdminModel();
+		$users = $admin->process();
+		$this->loadView('admin');
+		$this->data['users'] = $users;
+	}
+
+
+	public function actionAdd($params)
+	{
+		$user = $this->getModelFactory()->createUserModel();
+		try {
+			$user->register();
+		}
+		catch (PasswordsAreNotSameException $e) {
+			$this->alert($e->getMessage());
+		}
+		catch (DuplicateUser $e) {
+			$this->alert($e->getMessage());
+		}
+		catch (CompleteRegistrationException $e) {
+			$this->alert("");
+		}
+		$this->loadView('adminAddUser');
+	}
+
+
+	public function actionEdit($params)
+	{
+		$this->loadView('adminEditUser');
+	}
+
+
+	public function actionDelete($params)
+	{
 
 	}
+
 }
