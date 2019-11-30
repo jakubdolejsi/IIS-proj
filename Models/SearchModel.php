3d588e2ebd9eb8fdab9b5415f291084ec69bc85f
Tomáš Svěrák <xsvera04@stud.fit.vutbr.cz>
2019-11-30 21:22:13 +0100

    Vyhledavani - nadpis, serazeni dle casu

--- Models/SearchModel.php
+++ Models/SearchModel.php
@@ -26,7 +26,7 @@ class SearchModel extends BaseModel
         $date = date('Y-m-d');
         $query = 'select cw.name, cw.type, cw.genre, ce.date, ce.begin, h.label, ce.price from theatre.culture_event as ce 
 				join theatre.culture_work as cw on ce.id_culture_work = cw.id
-				join theatre.hall as h on ce.id_hall = h.id where ce.date >= ?';
+				join theatre.hall as h on ce.id_hall = h.id where ce.date >= ? order by ce.date asc, ce.begin asc';
 
         return $this->db->run($query, $date)->fetchAll(PDO::FETCH_ASSOC);
     }
